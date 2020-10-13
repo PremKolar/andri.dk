788f56b784907725d582c5fd3031fb2f3eb3b24d
Nikolaus Koopmann <nikolauskoopmann@gmail.com>
2020-10-13 10:15:50 +0200

    updates

---
+++ src/blog/2020/10/containers.md
@@ -0,0 +1,23 @@
+---
+path: "/blog/2019-07/containers"
+date: "2020-10-13T00:00:00.000Z"
+title: "Docker"
+tags: ["Docker"]
+cover: "./netlify.png"
+draft: false
+---
+
+My initial idea was to manually containerize this site and then host it at eg. Google Cloud.
+I had already watched the Frontend Masters course [Complete Intro to Containers](https://frontendmasters.com/courses/complete-intro-containers/) and was eager to apply my new skills.
+
+Then I found out about [Netlify](https://netlify.com) and couldn't resist... They make the whole process just too easy! 
+
+All I had to do was: 
+*   log in with my github account.
+*   select the branch i wanted to deploy.
+
+###
+After that, any commit I push to github triggers a fresh deploy.
+
+I had no idea how easy this would be :)
+
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,24 +4,29 @@ import andratar from "../../static/img/nikoBonfire.jpg";
 export const NavItem = ({ children, href }) => (
   <a
     href={href}
-    className="mt-4 lg:inline-block lg:mt-0 text-white hover:text-white-blue-300 mr-4"
+    className="mt-4 lg:inline-block lg:mt-0 text-black hover:text-white-blue-300 mr-4"
   >
     {children}
   </a>
 );
 
 export const Nav = (props) => (
-  <nav className="flex items-center justify-between flex-wrap bg-orange-600 p-6">
+  <nav
+    className="flex items-center justify-between flex-wrap p-6"
+    style={{
+      backgroundColor: "#e8de8c",
+    }}
+  >
     <a href="/" className="">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <img
+        {/* <img
           alt="avatar"
           src={andratar}
           className="rounded-full shadow-2xl w-8 h-8 mr-4 inline-block"
         />
         <span className="font-semibold text-xl tracking-tight inline-block">
           Niko
-        </span>
+        </span> */}
       </div>
     </a>
 
