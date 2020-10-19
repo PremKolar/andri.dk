7268cd2a7e1ac36f486f0fc1abdba5014d96b138
Nikolaus Koopmann <nikolauskoopmann@gmail.com>
2020-10-19 14:06:17 +0200

    getting filled, but still some bug.. got to get my boy from kindergarten now...

--- src/pages/sudokusolver.jsx
+++ src/pages/sudokusolver.jsx
@@ -17,16 +17,23 @@ class SudokuComponent extends Component {
 class SudokuSolver extends SudokuComponent {
   constructor(props) {
     super(props);
+    this.sudoku.SudokuSolver = this;
   }
   render() {
     return (
       <div className="sudoku-page">
-        <TheFullField sudoku={this.sudoku} />
+        <TheFullField ref="full-field" sudoku={this.sudoku} />
         <div className="goButton" style={{ marginLeft: "auto" }}>
-          <button>solve!</button>
+          <button onClick={this.solveCallback.bind(this)}>solve!</button>
         </div>
       </div>
     );
+  }
+  solveCallback(e) {
+    this.sudoku.solve();
+  }
+  updateValue(row, col, v) {
+    this.refs["full-field"].updateValue(row, col, v);
   }
 }
 
@@ -35,16 +42,31 @@ class TheFullField extends SudokuComponent {
     return (
       <div className="three-columns-of-ThreeThreeByThrees">
         <div>
-          <ThreeThreeByThrees sudoku={this.sudoku} rootColumn="0" />
+          <ThreeThreeByThrees
+            ref="33x3s0"
+            sudoku={this.sudoku}
+            rootColumn="0"
+          />
         </div>
         <div>
-          <ThreeThreeByThrees sudoku={this.sudoku} rootColumn="3" />
+          <ThreeThreeByThrees
+            ref="33x3s1"
+            sudoku={this.sudoku}
+            rootColumn="3"
+          />
         </div>
         <div>
-          <ThreeThreeByThrees sudoku={this.sudoku} rootColumn="6" />
+          <ThreeThreeByThrees
+            ref="33x3s2"
+            sudoku={this.sudoku}
+            rootColumn="6"
+          />
         </div>
       </div>
     );
+  }
+  updateValue(row, col, v) {
+    this.refs["33x3s" + Math.floor(col / 3)].updateValue(row, col, v);
   }
 }
 
@@ -57,22 +79,28 @@ class ThreeThreeByThrees extends SudokuComponent {
     return (
       <div>
         <ThreeByThree
+          ref="3x30"
           sudoku={this.sudoku}
           rootColumn={this.rootColumn}
           rootRow="0"
         />
         <ThreeByThree
+          ref="3x31"
           sudoku={this.sudoku}
           rootColumn={this.rootColumn}
           rootRow="3"
         />
         <ThreeByThree
+          ref="3x32"
           sudoku={this.sudoku}
           rootColumn={this.rootColumn}
           rootRow="6"
         />
       </div>
     );
+  }
+  updateValue(row, col, v) {
+    this.refs["3x3" + Math.floor(row / 3)].updateValue(row, col, v);
   }
 }
 
@@ -87,16 +115,19 @@ class ThreeByThree extends SudokuComponent {
       <div className="three-by-three">
         <div className="num-row">
           <NumBox
+            ref="00"
             sudoku={this.sudoku}
             row={this.rootRow + 0}
             column={this.rootColumn + 0}
           />
           <NumBox
+            ref="01"
             sudoku={this.sudoku}
             row={this.rootRow + 0}
             column={this.rootColumn + 1}
           />
           <NumBox
+            ref="02"
             sudoku={this.sudoku}
             row={this.rootRow + 0}
             column={this.rootColumn + 2}
@@ -104,16 +135,19 @@ class ThreeByThree extends SudokuComponent {
         </div>
         <div className="num-row">
           <NumBox
+            ref="10"
             sudoku={this.sudoku}
             row={this.rootRow + 1}
             column={this.rootColumn + 0}
           />
           <NumBox
+            ref="11"
             sudoku={this.sudoku}
             row={this.rootRow + 1}
             column={this.rootColumn + 1}
           />
           <NumBox
+            ref="12"
             sudoku={this.sudoku}
             row={this.rootRow + 1}
             column={this.rootColumn + 2}
@@ -121,16 +155,19 @@ class ThreeByThree extends SudokuComponent {
         </div>
         <div className="num-row">
           <NumBox
+            ref="20"
             sudoku={this.sudoku}
             row={this.rootRow + 2}
             column={this.rootColumn + 0}
           />
           <NumBox
+            ref="21"
             sudoku={this.sudoku}
             row={this.rootRow + 2}
             column={this.rootColumn + 1}
           />
           <NumBox
+            ref="22"
             sudoku={this.sudoku}
             row={this.rootRow + 2}
             column={this.rootColumn + 2}
@@ -138,6 +175,9 @@ class ThreeByThree extends SudokuComponent {
         </div>
       </div>
     );
+  }
+  updateValue(row, col, v) {
+    this.refs["" + (col % 3) + (row % 3)].updateValue(v);
   }
 }
 
@@ -155,11 +195,14 @@ class NumBox extends SudokuComponent {
         min="0"
         max="9"
         placeholder=""
+        ref="input"
         onChange={this.numberEnteredCallback.bind(this)}
       />
     );
   }
-
+  updateValue(v) {
+    this.refs["input"].value = v;
+  }
   numberEnteredCallback(e) {
     if (e.target.value < 1 || e.target.value > 9) {
       e.target.value = "";
@@ -178,14 +221,50 @@ class Sudoku {
     .fill()
     .map(() => Array(9).fill(0));
 
+  solve() {
+    for (let col = 0; col < 9; col++) {
+      for (let row = 0; row < 9; row++) {
+        if (this.get(row, col) > 0) {
+          continue;
+        }
+        for (let v = 1; v <= 9; v++) {
+          if (!this.valueIsPossible(row, col, v)) {
+            continue;
+          }
+          this.setAndUpdateUI(row, col, v);
+          if (this.solve()) {
+            return true;
+          }
+        }
+        this.setAndUpdateUI(row, col, 0);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  setAndUpdateUI(row, col, v) {
+    this.set(row, col, v);
+    this.SudokuSolver.updateValue(row, col, v);
+  }
+
   set(row, col, v) {
     assert(row >= 0 && row < 9);
     assert(col >= 0 && col < 9);
-    assert(this.valueIsPossible(row, col, v));
+    if (!this.valueIsPossible(row, col, v)) {
+      row;
+    }
     this.mtrx[row][col] = v;
   }
 
+  get(row, col) {
+    return this.mtrx[row][col];
+  }
+
   valueIsPossible(row, col, v) {
+    console.log(this.valueIsOKvertically(row, col, v));
+    console.log(this.valueIsOKhorizontally(row, col, v));
+    console.log(this.valueIsOKinSquare(row, col, v));
     return (
       this.valueIsOKvertically(row, col, v) &&
       this.valueIsOKhorizontally(row, col, v) &&
@@ -203,12 +282,14 @@ class Sudoku {
     });
   }
 
-  twoDtoLinIdx(row, col) {}
+  twoDtoLinIdx(row, col) {
+    return Math.floor(row / 3) * ((col % 3 )+ 1) + (row % 3);
+  }
 
   valueIsOKinSquare(row, col, v) {
     let vec = [].concat.apply([], this.getValuesInRelevantSquare(row, col));
     let idx = this.twoDtoLinIdx(row, col);
-    return this._valueIsOKinVector(vec, col, v);
+    return this._valueIsOKinVector(vec, idx, v);
   }
 
   _valueIsOKinVector(vec, idx, v) {
